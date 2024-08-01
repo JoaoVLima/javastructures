02ec911ca09f3688be6ba8cf06e10eaf939c3eaa
JoaoVLima <celu.joaolima@gmail.com>
2024-08-01 10:56:28 -0300

    Pilha no main

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        // Pilha pilha = new Pilha(10);
+        // pilha.imprime();
+        // pilha.insere(1);
+        // pilha.insere(2);
+        // pilha.insere(3);
+        // pilha.insere(4);
+        // pilha.insere(5);
+        // pilha.imprime();
+        // pilha.insere(6);
+        // pilha.insere(7);
+        // pilha.insere(8);
+        // pilha.insere(9);
+        // pilha.insere(10);
+        // pilha.imprime();
+        // pilha.remove();
+        // pilha.remove();
+        // pilha.remove();
+        // pilha.insere(11);
+        // pilha.insere(12);
+        // pilha.insere(13);
+        // pilha.imprime();
+        // pilha.remove();
+        // pilha.remove();
+        // pilha.remove();
+        // pilha.remove();
+        // pilha.imprime();
+
+
     }
 }
